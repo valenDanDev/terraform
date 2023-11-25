44c0c6dfbb38e6c681271af4f90f8550be296f41
Daniel Valencia G <valenciadaniel8022015@gmail.com>
2023-11-24 20:09:08 -0500

    update main.tf

--- main.tf
+++ main.tf
@@ -321,7 +321,182 @@ yum install -y php php-common php-pear
 yum install -y php-{cgi,curl,mbstring,gd,mysqlnd,gettext,json,xml,fpm,intl,zip}
 mkdir /var/www/html/myapp
 cd /var/www/html/myapp
-echo "<h1>Hello, World!</h1>" > index.php
+    echo '<!DOCTYPE html>
+    <head>
+        <title>Cell-Land</title>
+        <link rel="stylesheet" type="text/css" href="misEstilos.css">
+        <script src="https://unpkg.com/axios@1.3.4/dist/axios.min.js"></script>
+        <script src="misScripts.js"></script>
+    </head>
+    <body onload="obtenerTodos()">
+        <div class="titulo">
+            <h1>Cell-Land</h1>
+            <img src="logo.png" alt="Icono">
+        </div>
+         <hr class="linea"/>
+      <div>
+          <h2>Administración de productos</h2>
+          <table>
+              <thead>
+                  <tr>
+                      <th>ID</th>
+                      <th>Nombre</th>
+                      <th>Modelo</th>
+                      <th>Color</th>
+                      <th>Almacenamiento</th>
+                      <th>Procesador</th>
+                  </tr>
+              </thead>    
+              <tbody id="tblContenido">
+              </tbody>
+          </table>
+      </div>
+      <div>
+          <div>
+              <h4>Consultar artículo</h4>
+              <input type="numer" id="txtConsulta" placeholder="Ingrese su ID">
+              <button onclick="consultarUno()">Consultar</button><br/><br/>
+          </div>
+          <div>
+              <label for="consultaNombre">Nombre:</label>
+              <textarea id="consultaNombre" rows="1" cols="30"></textarea>
+          </div>
+          <div>
+              <label for="consultaModelo">Modelo:</label>
+              <textarea id="consultaModelo" rows="1" cols="30"></textarea>
+          </div>
+          <div>
+              <label for="consultaColor">Color:</label>
+              <textarea id="consultaColor" rows="1" cols="30"></textarea>
+          </div>
+          <div>
+              <label for="consultaAlmacenamiento">Almacenamiento:</label>
+              <textarea id="consultaAlmacenamiento" rows="1" cols="30"></textarea>
+          </div>
+          <div>
+              <label for="consultaProcesador">Procesador:</label>
+              <textarea id="consultaProcesador" rows="1" cols="30"></textarea>
+          </div><br/>
+          <button onclick="modificarUno()">Modificar</button>
+          <button onclick="eliminarUno()">Eliminar</button>
+      </div>
+      <div>
+          <div>
+              <h4>Agregar artículo</h4>
+              <input type="text" id="inputMarca" placeholder="Marca">
+              <input type="text" id="inputModelo" placeholder="Modelo">
+              <input type="text" id="inputColor" placeholder="Color">
+              <input type="text" id="inputAlmacenamiento" placeholder="Almacenamiento (GB)">
+              <input type="text" id="inputProcesador" placeholder="Procesador"><br/><br/>
+              <button onclick="agregarUno()">Agregar Nuevo</button>
+          </div>
+      </div>
+    </body>
+    </html>' > /var/www/html/myapp/index.html
+      echo 'body {
+    width: 80%;
+    margin: 0 auto;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    background-color: #D0D977;
+}
+
+.titulo {
+    display: flex;
+    justify-content: center;
+}
+
+h1 {
+    position: absolute;
+    margin-top: 12px;
+    border: 1px solid #ddd;
+    border-radius: 10px;
+    padding: 5px;
+    background-color: #FFFFE0;
+    box-shadow: 0 2px 2px rgba(0, 0, 0, 0.1);
+    text-align: center;
+}
+
+img {
+    position: relative;
+    width: 100px;
+    margin-left: 220px;
+}
+
+.linea {
+    margin-top: -1px;
+    border-color: black;
+}
+
+h2 {
+    text-align: center;
+}
+
+h4 {
+    font-size: 18px;
+    text-decoration: underline;
+}
+
+table {
+    border-collapse: collapse;
+    width: 100%;
+    margin: 10px 0;
+}
+
+th, td {
+    border: 1px solid #ddd;
+    padding: 8px; 
+    text-align: left;
+}
+
+th {
+    background-color: darkolivegreen;
+    font-weight: bold;
+    text-align: center;
+    color: white;
+}
+
+td {
+    background-color: #FFFFE0;
+}
+
+input {
+    background-color: #FFFFE0;
+    padding: 10px 10px;
+    font-size: 15px;
+    width: 150px;
+    border: none;
+    border-bottom: solid 1px;
+}
+
+button {
+    background-color: darkolivegreen;
+    color: white;
+    height: 35px;
+    font-size: 15px;
+    width: 170px;
+    cursor: pointer;
+}
+
+label {
+    display: inline-block;
+    width: 130px; /* ajusta el ancho según tus necesidades */
+    vertical-align: top;
+    line-height: 50px;
+}
+
+textarea {
+    background-color: #f7f7f7;
+    border: 1px solid #ccc;
+    box-shadow: 1px 1px 2px #ccc;
+    cursor: text;
+    font-family: inherit;
+    font-size: 100%;
+    padding: 8px;
+    resize: none;
+    text-align: left;
+    height: 30px;
+}
+' > /var/www/html/myapp/misEstilos.css
 chown -R apache:apache /var/www/html/myapp
 chmod -R 755 /var/www/html/myapp
 chmod -R 777 /var/www/
